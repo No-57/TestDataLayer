7098ca06cd35a17d802f5fca0217a80574d2fad3
Vince <vinceku@pklotcorp.com>
2023-08-07 15:14:12 +0800

    add fake core data object

--- Sources/TestDataLayer/TestDataLayer.swift
+++ Sources/TestDataLayer/TestDataLayer.swift
@@ -9,3 +9,11 @@ public struct TestTT {
     public let name: String = "Hi"
     public init() {}
 }
+
+import Foundation
+import CoreData
+
+@objc(Product_Core_Data)
+public class Product_Core_Data: NSManagedObject {
+
+}
