065c0cb7be89e9f7d6372e38a89581c9fdd659e5
Vince <vinceku@pklotcorp.com>
2023-08-07 15:40:16 +0800

    update

---
+++ Sources/TestDataLayer/Product.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 辜敬閎 on 2023/8/7.
+//
+
+import Foundation
---
+++ Sources/TestDataLayer/TestTT.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by 辜敬閎 on 2023/8/7.
+//
+
+import Foundation
+
+public struct TestTT {
+    public let name: String = "Hi"
+    public init() {}
+}
